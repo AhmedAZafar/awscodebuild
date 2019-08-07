91fb692040e941cee129ffb4b6a48948fe79ac58
AhmedAZafar <engr.ahmedzafar@protonmail.com>
2019-08-07 12:20:59 +0100

    Final test

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo File:$0 Args: $1 $2 $3 
 
+USERNAME=$(aws --region=eu-west-1 ssm get-parameter --name "test-nuget-username" --with-decryption --output text --query Parameter.Value)
+PASSWORD=$(aws --region=eu-west-1 ssm get-parameter --name "test-nuget-password" --with-decryption --output text --query Parameter.Value)
+FEED_KEY=$(aws --region=eu-west-1 ssm get-parameter --name "test-nuget-feed-key" --with-decryption --output text --query Parameter.Value)
+FEED_VALUE=$(aws --region=eu-west-1 ssm get-parameter --name "test-nuget-feed-value" --with-decryption --output text --query Parameter.Value)
+
 > nuget.config
 
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><configuration><packageSources><add key=\"vsts\" value=\"$1\" /></packageSources><packageSourceCredentials><vsts><add key=\"Username\" value=\"$2\" /><add key=\"ClearTextPassword\" value=\"$3\" /></vsts></packageSourceCredentials></configuration>" >> nuget.config
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><configuration><packageSources><add key=\"$FEED_KEY\" value=\"$FEED_VALUE\" /></packageSources><packageSourceCredentials><$FEED_KEY><add key=\"Username\" value=\"$USERNAME\" /><add key=\"ClearTextPassword\" value=\"$PASSWORD\" /></$FEED_KEY></packageSourceCredentials></configuration>" >> nuget.config
 
 cat nuget.config
